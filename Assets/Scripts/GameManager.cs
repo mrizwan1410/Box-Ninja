bf2efc6c6c0154e091664e21158fdfe630f665e1
RIZWAN <morizwan1410@gmail.com>
2025-11-15 19:27:56 +0530

    restart game

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,6 +11,7 @@ public class GameManager : MonoBehaviour
     private int score;
     public bool isGameActive;
 
+    public Button restartButton;
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI gameOverText;
     public List<GameObject> targets;
@@ -30,7 +33,13 @@ public class GameManager : MonoBehaviour
     public void GameOver()
     {
         isGameActive = false;
+        restartButton.gameObject.SetActive(true);
         gameOverText.gameObject.SetActive(true);
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
     IEnumerator SpawnTarget()
